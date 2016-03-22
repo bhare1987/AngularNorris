c627f2519ed5cd5a65af3b23016247e1fd14ee40
bhare1987 <brandonlhare@gmail.com>
2016-03-22 15:03:12 -0400

    got norris working

--- app.controller.js
+++ app.controller.js
@@ -4,7 +4,26 @@ angular
 
 
   })
-  .controller('norrisController', function($scope){
+  .controller('norrisController', function($scope, $log, norrisService){
+    $scope.norrisJoke = "";
+    $scope.userJoke = "";
 
-    
-  })
+
+    $scope.getNorrisJokeCtrl = function(){
+      $log.debug('this works');
+      norrisService.getNorrisJoke()
+        .then(function(data){
+          $log.debug(data);
+          $scope.norrisJoke = data.data.value.joke;
+        });
+    }
+
+    $scope.getUserJokeCtrl = function(firstName, lastName){
+      $log.debug('this works');
+      norrisService.getUserJoke(firstName, lastName)
+        .then(function(data){
+          $scope.userJoke = data.data.value.joke;
+        });
+    }
+
+  });
--- app.service.js
+++ app.service.js
@@ -2,10 +2,18 @@ angular
   .module('AngularNorris')
   .service('norrisService', function($http){
 
-
-
+    var norrisService = {
+      baseUrl: 'http://api.icndb.com/',
+      getNorrisJoke: function(){
+        return $http.get(norrisService.baseUrl + 'jokes/random');
+      },
+      getUserJoke: function(firstName, lastName){
+        return $http.get(norrisService.baseUrl + 'jokes/random?firstName=' + firstName + '&lastName=' + lastName);
+      }
+    }
 
     return {
-      
+      getNorrisJoke: norrisService.getNorrisJoke,
+      getUserJoke: norrisService.getUserJoke
     }
   })
